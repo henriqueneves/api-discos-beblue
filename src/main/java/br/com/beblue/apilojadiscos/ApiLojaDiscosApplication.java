0b694a61ec2851145bd445258c781d50cc329d15
Henrique Neves <henrique.neves@beblue.com.br>
2019-06-06 16:44:00 -0300

    Spring boot create project

---
+++ src/main/java/br/com/beblue/apilojadiscos/ApiLojaDiscosApplication.java
@@ -0,0 +1,13 @@
+package br.com.beblue.apilojadiscos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiLojaDiscosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiLojaDiscosApplication.class, args);
+	}
+
+}
